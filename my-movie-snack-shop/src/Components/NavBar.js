f6b7dc4e3b6eb16efca092d929832e44bdc9000b
Wato Elle <watoelle@gmail.com>
2024-09-12 10:44:07 +0300

    db.json running

--- my-movie-snack-shop/src/Components/NavBar.js
+++ my-movie-snack-shop/src/Components/NavBar.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import './NavBar.css'; // Make sure you create this CSS file to style your navbar
+//import './NavBar.css'; // Import your CSS for styling
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart</Link>
-        </li>
-        <li>
-          <Link to="/checkout">Checkout</Link>
-        </li>
-        <li>
-          <Link to="/order-history">Order History</Link>
-        </li>
-      </ul>
+      <div className="container">
+        <h1 className="logo">Movie Snack Shop</h1>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/checkout">Checkout</Link>
+          </li>
+          <li>
+            <Link to="/order-history">Order History</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
